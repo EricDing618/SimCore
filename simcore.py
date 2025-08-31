04c264130bc15c37aa492cae6ce3695fe3f9b525
Eric学编程 <1843794003@qq.com>
2025-08-31 16:28:17 +0800

    rm useless code

--- simcore.py
+++ simcore.py
@@ -2,12 +2,3 @@ from src.Elevator.demo import demo
 
 if __name__ == "__main__":
     demo()
-    '''a=[1,2,3]
-    for i in a:
-        match i:
-            case 1:
-                print(1.1)
-            case 1|2:
-                print(1)
-            case 3:
-                print(3)'''
